1d1c362592be4261926e610e26b7209d585911dd
Ben Drucker <bvdrucker@gmail.com>
2015-06-04 18:00:40 -0400

    Scaffold test runner

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 'use strict'
 
 var test = require('tape')
+var browserify = require('browserify')
 var mainify = require('./')
 
 test(function (t) {
-
+  function bundle (scenario, callback) {
+    t.test(scenario, planned, function (t) {
+      t.plan(planned)
+      browserify()
+        .add(__dirname + '/fixtures/' + scenario)
+        .transform(mainify)
+        .bundle(function (err, code) {
+          if (err) return t.end(err)
+          callback(t, code)
+        })
+    })
+  }
 })
